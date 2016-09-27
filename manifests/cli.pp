c6d511182b65a4a8d3dc26b956e352e3a80b4bdd
John Florian <jflorian@doubledog.org>
2016-09-27 10:48:05 -0400

    New - parameters for handling Koji CLI call failures
    This commit exposes through Puppet the following parameters to govern
    how the Koji CLI handles call failures to the Koji Hub:
    
        - max_retries
        - offline_retry
        - offline_retry_interval
        - retry_interval

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -22,6 +22,28 @@
 #
 # ==== Optional
 #
+# [*max_retries*]
+#   When making Koji calls, if the Koji Hub reports a temporary failure, how
+#   many times should the call be retried?  The default is 30.
+#
+# [*offline_retry*]
+#   When making Koji calls, if the Koji Hub reports itself as offline, should
+#   the call be retried automatically?  The default is false.
+#
+#   Note that offline failures are treated specially from other temporary
+#   failures.  These are not constrained by other failure handling options,
+#   most notably "max_retries".
+#
+# [*offline_retry_interval*]
+#   When making Koji calls, if the Koji Hub reports itself as offline and
+#   "offline_retry" is true, this determines how many seconds the Koji Client
+#   will wait before attempting the call again.  The default is 20 seconds.
+#
+# [*retry_interval*]
+#   When making Koji calls, if the Koji Hub reports a temporary failure, this
+#   determines how many seconds the Koji Client will wait before attempting
+#   the call again.  The default is 20 seconds.
+#
 # === Authors
 #
 #   John Florian <jflorian@doubledog.org>
@@ -36,7 +58,16 @@ class koji::cli (
         $hub,
         $top_dir,
         $web,
+        $max_retries=30,
+        $offline_retry=false,
+        $offline_retry_interval=20,
+        $retry_interval=20,
     ) inherits ::koji::params {
+
+    validate_bool($offline_retry)
+    validate_integer($max_retries)
+    validate_integer($offline_retry_interval)
+    validate_integer($retry_interval)
 
     package { $::koji::params::cli_packages:
         ensure  => installed,
